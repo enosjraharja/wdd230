502e54287f131e0f35842371d86e3e6b3bd0107c
Ilham Bahtiar <bahtiari073@gmail.com>
2022-02-01 23:25:55 +0800

    fix about page

--- landing_page/assets/js/main.js
+++ landing_page/assets/js/main.js
@@ -1,53 +1,58 @@
+let bannerPrev, bannerNext
+
 const toggleMenu = () => document.querySelector('ul.menu').classList.toggle('hide')
 
-const banner = tns({
-	container: ".banner-items",
-	controls: false,
-	mouseDrag: true,
-	autoplay: true,
-	autoplayButton: false,
-	autoplayButtonOutput: false,
-	autoplayHoverPause: true,
-	nav: false,
-})
+if (document.querySelector('.banner-items') != undefined) {
+  const banner = tns({
+    container: '.banner-items',
+    controls: false,
+    mouseDrag: true,
+    autoplay: true,
+    autoplayButton: false,
+    autoplayButtonOutput: false,
+    autoplayHoverPause: true,
+    nav: false,
+  })
+
+  bannerPrev = () => banner.goTo('prev')
+  bannerNext = () => banner.goTo('next')
+}
 
 const testimonials = tns({
-	container: ".testimonial-items",
-	controls: false,
-	mouseDrag: true,
-	autoplay: true,
-	autoplayButton: false,
-	autoplayButtonOutput: false, autoplayHoverPause: true,
-	nav: false,
-	responsive: {
-		640: {
-			items: 2,
-		}
-	}
+  container: '.testimonial-items',
+  controls: false,
+  mouseDrag: true,
+  autoplay: true,
+  autoplayButton: false,
+  autoplayButtonOutput: false,
+  autoplayHoverPause: true,
+  nav: false,
+  responsive: {
+    640: {
+      items: 2,
+    },
+  },
 })
 const partners = tns({
-	container: ".partner-items",
-	controls: false,
-	mouseDrag: true,
-	autoplay: true,
-	autoplayButton: false,
-	autoplayButtonOutput: false,
-	autoplayHoverPause: true,
-	nav: false,
-	responsive: {
-		640: {
-			items: 3,
-		}
-	}
+  container: '.partner-items',
+  controls: false,
+  mouseDrag: true,
+  autoplay: true,
+  autoplayButton: false,
+  autoplayButtonOutput: false,
+  autoplayHoverPause: true,
+  nav: false,
+  responsive: {
+    640: {
+      items: 3,
+    },
+  },
 })
 
-const bannerPrev = () => banner.goTo("prev")
-const bannerNext = () => banner.goTo("next")
-
 window.addEventListener('scroll', () => {
-	if (window.scrollY > 200) {
-		document.querySelector('header').classList.add('fixed')
-	} else {
-		document.querySelector('header').classList.remove('fixed')
-	}
+  if (window.scrollY > 200) {
+    document.querySelector('header').classList.add('fixed')
+  } else {
+    document.querySelector('header').classList.remove('fixed')
+  }
 })
